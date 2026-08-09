08548447ba6077bb61d692999809501e71b2ad50
Mahmoud Ragab <sendbad20006@gmail.com>
2026-08-09 05:30:53 +0300

    add todo generating function to quik project and paginator test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,6 +12,9 @@ import Input from "../components/ui/Input";
 import Modal from "../components/ui/Modal";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import api from "../config/axios.config";
+import Chance from 'chance'
+
+
 
 function Home() {
 
@@ -24,6 +27,7 @@ function Home() {
   }
 
   const queryClient = useQueryClient()
+  const chance = Chance()
 
 
   const storageKey = "userData"
@@ -78,8 +82,6 @@ function Home() {
     reset()
   }
 
-
-
   const { isPending, data, error } = useAuthenticatedQuery(
     ['todos'],
     "/users/me?populate=todos",
@@ -90,9 +92,42 @@ function Home() {
     })
 
 
+  const { mutate: generateTodos, isPending: isGenerating } = useMutation({
+    mutationFn: async () => {
+      const promises = [];
+
+      for (let i = 0; i < 100; i++) {
+        const request = api.post(
+          `/todos`,
+          {
+            data: {
+              title: chance.sentence({ words: 5 }),
+              description: chance.paragraph({ sentences: 2 }),
+            },
+          },
+          {
+            headers: { Authorization: `Bearer ${userData.jwt}` },
+          }
+        );
+        promises.push(request);
+      }
+
+      return await Promise.all(promises);
+    },
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['todos'] });
+    },
+
+    onError: (error) => {
+      console.log(error);
+    },
+  });
 
 
-  if (isPending) return (
+
+
+  if (isPending || isGenerating) return (
     <div role="status" className="max-w-sm animate-pulse mx-auto">
       <div className="h-2.5 bg-gray-100 rounded-full w-48 mb-4"></div>
       <div className="h-2 bg-gray-100  rounded-full max-w-\[360px\] mb-2.5"></div>
@@ -110,7 +145,10 @@ function Home() {
   return (
     <>
       <div className="w-100 md:w-150 lg:w-200 mx-auto">
-        <Button className="btn block mx-auto my-4" onClick={openAddModal}>Post new todo</Button>
+        <div className="flex justify-center items-center gap-8 my-8">
+          <Button className="btn" onClick={openAddModal}>Post new todo</Button>
+          <Button className="btn btn-outline" onClick={() => generateTodos()}>Generate todos (100)</Button>
+        </div>
         <TodoList todos={data.todos} jwt={userData.jwt} />
       </div>
       <Modal isOpen={isAddOpen} title="Edit Todo">
